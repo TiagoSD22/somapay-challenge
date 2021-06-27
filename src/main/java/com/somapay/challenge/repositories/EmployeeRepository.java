c296a21ba77e553a684f0cf48c82ee5e10bfae8f
Tiago Siqueira Dionizio <tiagosd@pop-os.localdomain>
2021-06-27 20:02:20 -0300

    adicionando repositorio para funcionario

--- src/main/java/com/somapay/challenge/repositories/EmployeeRepository.java
+++ src/main/java/com/somapay/challenge/repositories/EmployeeRepository.java
@@ -1,2 +1,8 @@
-package com.somapay.challenge.repositories;public interface EmployeeRepository {
+package com.somapay.challenge.repositories;
+
+import com.somapay.challenge.entities.Company;
+import com.somapay.challenge.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
