bd3de4f7f79b7d186687012065f6e2f1a4c5ec3b
Tiago Siqueira Dionizio <tiagosd@pop-os.localdomain>
2021-06-27 20:01:03 -0300

    adicionando rotas para cadastrar funcionario e obter saldo de funcionario

--- src/main/java/com/somapay/challenge/controllers/EmployeeController.java
+++ src/main/java/com/somapay/challenge/controllers/EmployeeController.java
@@ -1,2 +1,36 @@
-package com.somapay.challenge.controllers;public class EmployeeController {
+package com.somapay.challenge.controllers;
+
+import com.somapay.challenge.entities.Company;
+import com.somapay.challenge.entities.Employee;
+import com.somapay.challenge.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/employee-api")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository repository;
+
+    @PostMapping("/create")
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
+        this.repository.save(employee);
+        return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+    }
+
+    @GetMapping("/get-account-balance/{id}")
+    public ResponseEntity<Float> getAccountBalance(@PathVariable Integer id){
+        Optional<Employee> employee = this.repository.findById(id);
+
+        if(employee.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("employee with id %d not found", id));
+        }
+
+        return new ResponseEntity<Float>(employee.get().getAccountBalance(), HttpStatus.OK);
+    }
 }
