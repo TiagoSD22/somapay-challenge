af2dab2acf334d00012da1c112a378fddc8dd525
Tiago Siqueira Dionizio <tiagosd@pop-os.localdomain>
2021-06-27 20:02:05 -0300

    adicionando modelo de funcionario

--- src/main/java/com/somapay/challenge/entities/Employee.java
+++ src/main/java/com/somapay/challenge/entities/Employee.java
@@ -1,2 +1,49 @@
-package com.somapay.challenge.entities;public class Employee {
+package com.somapay.challenge.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "account_balance", nullable = false, columnDefinition = "float default 0")
+    private float accountBalance;
+
+    @ManyToOne()
+    @JoinColumn(name="company_id")
+    private Company company;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(float accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 }
