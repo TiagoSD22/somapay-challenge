9d1c15a47880a10b0131c3eeba81557806f15381
Tiago Siqueira Dionizio <tiagosd@pop-os.localdomain>
2021-06-27 23:17:37 -0300

    adicionando rota para transferir dinheiro entre conta de empresa e funcionario

--- src/main/java/com/somapay/challenge/controllers/CompanyController.java
+++ src/main/java/com/somapay/challenge/controllers/CompanyController.java
@@ -1,7 +1,10 @@
 package com.somapay.challenge.controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.somapay.challenge.entities.Company;
+import com.somapay.challenge.entities.Employee;
 import com.somapay.challenge.repositories.CompanyRepository;
+import com.somapay.challenge.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,9 @@ import java.util.Optional;
 public class CompanyController {
     @Autowired
     private CompanyRepository repository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @PostMapping("/create")
     public ResponseEntity<Company> createCompany(@RequestBody Company company){
@@ -44,5 +50,42 @@ public class CompanyController {
     public ResponseEntity<List<Company>> getAll(){
         List<Company> companyList = this.repository.findAll();
         return new ResponseEntity<List<Company>>(companyList, HttpStatus.OK);
+    }
+
+    @PostMapping("/transfer-money/{companyId}/{employeeId}")
+    public ResponseEntity<String> transferMoneyFromCompanyAccountToEmployee
+            (@PathVariable Integer companyId, @PathVariable Integer employeeId, @RequestBody ObjectNode payload){
+
+        Optional<Company> companySnap = this.repository.findById(companyId);
+
+        if(companySnap.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("company with id %d not found", companyId));
+        }
+
+        Optional<Employee> employeeSnap = this.employeeRepository.findById(employeeId);
+
+        if(employeeSnap.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("employee with id %d not found", employeeId));
+        }
+
+        Company company = companySnap.get();
+        Employee employee = employeeSnap.get();
+        float value = (float) payload.get("value").asDouble();
+
+        if(value > company.getAccountBalance()){
+            return new ResponseEntity<String>("Saldo na conta inferior ao valor solicitado para tansferencia",
+                    HttpStatus.OK);
+        }
+
+        company.setAccountBalance(company.getAccountBalance() - value);
+        employee.setAccountBalance(employee.getAccountBalance() + value);
+
+        repository.save(company);
+        employeeRepository.save(employee);
+
+        return new ResponseEntity<String>("Transferencia realizada com sucesso!",
+                HttpStatus.OK);
     }
 }
